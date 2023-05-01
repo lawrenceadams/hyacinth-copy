36b65d9e1fcb8aa36bd2f29fb559ed1c82cf93b9
hqva <vaidya@hrish.ee>
2023-05-01 17:53:06 +0000

    commit before deelets

--- app/pages/discharges/callbacks.py
+++ app/pages/discharges/callbacks.py
@@ -1,42 +1,53 @@
-import pandas as pd
-from databases import odbc_cursor, cosmos_client, CosmosDBLongCallbackManager
-from dash import Input, Output, callback
-from pathlib import Path
-from pages.discharges import CAMPUSES
-from datetime import datetime, timedelta
 import os
-from cachetools import TTLCache
-from uuid import uuid4
-import diskcache
-from azure.cosmos import PartitionKey
-from ids import STORE_TIMER_5M
 import logging
+import pandas as pd
+from pathlib import Path
+from datetime import datetime, timedelta
+
+from dash import Input, Output, callback
+
+from databases import odbc_cursor, cosmos_client, CosmosDBLongCallbackManager
+from ids import STORE_TIMER_5M
+
+AUTO_REFRESH_INTERVAL = 60 * 5
+
+# Read SQL queries
+DISCHARGES_QUERY = (Path(__file__).parent / "sql/discharges.sql").read_text()
+DEV_QUERY = (Path(__file__).parent / "sql/app-dev.sql").read_text()
+
+# Auto-refresh triggered
+cosmosdb_callback_manager = CosmosDBLongCallbackManager(
+    cosmos_client(),
+    database_name="hyacinth-state",
+    container_name="hyacinth",
+    expire=AUTO_REFRESH_INTERVAL,
+    partition_key="/Id",
+)
 
 
-auto_refresh_interval = 60 * 5
-
-
+# Fetch discharges data based on environment
 def _fetch_discharges():
+    logging.info("Fetching discharges from SQL store")
     if os.environ["ENVIRONMENT"] == "prod":
-        query = (Path(__file__).parent / "sql/discharges.sql").read_text()
-        data = pd.read_sql(query, odbc_cursor().connection)
+        data = pd.read_sql(DISCHARGES_QUERY, odbc_cursor().connection)
         beds = pd.read_json("assets/locations/bed_defaults.json")
         df = data.merge(
             beds[["location_string", "department", "room", "location_name"]],
             how="left",
             left_on="hl7_location",
             right_on="location_string",
-        )  # .query("department == @dept_selector"))
+        )
     else:
-        query = (Path(__file__).parent / "sql/app-dev.sql").read_text()
-        df = pd.read_sql(query, odbc_cursor().connection)
+        df = pd.read_sql(DEV_QUERY, odbc_cursor().connection)
         df.columns = ["mrn", "firstname", "sex", "department"]
+    logging.info(f"Fetched {len(df)} rows from SQL store")
     return df
 
 
+# Callback function to get discharges data
 @callback(
     Output("discharges_table", "data"),
-    Output("last_updated_time", "children"),
+    Output("refresh_button", "children"),
     Output("refresh_button", "loading"),
     Input("refresh_button", "n_clicks"),
     Input(STORE_TIMER_5M, "n_intervals"),
@@ -44,59 +55,47 @@ def _fetch_discharges():
 def _get_discharges(n_clicks, n_intervals):
     nc = 0
     now = datetime.now()
-    if nc != n_clicks:  # triggering a "refresh"
-        logging.info("")
+
+    # Manual refresh triggered
+    if n_clicks and n_clicks > nc:
+        logging.info("Manual refresh triggered...")
+
         df = _fetch_discharges()
         nc = n_clicks
-        return df, f"Updated manually at {now:%H:%M:%S}", True
 
-    else:  # nc == n_clicks; i.e. interval only
-        cosmosdb_callback_manager = CosmosDBLongCallbackManager(
-            cosmos_client(),
-            database_name="hyacinth-state",
-            container_name="hyacinth",
-            expire=auto_refresh_interval,
-            partition_key="/Id",
+        logging.info("Refreshing cached data...")
+        cosmosdb_callback_manager.set(
+            cache_key="discharges_cache", value=df.to_dict("records")
         )
 
-        cached_data = cosmosdb_callback_manager.get("discharges_cache")
-        last_updated = cosmosdb_callback_manager.get("last_updated")
+        cosmosdb_callback_manager.set(cache_key="last_updated", value=now)
+        return df.to_dict("records"), f"Updated manually at {now:%H:%M:%S}", True
 
-        if last_updated:  # is not null
-            last_updated = datetime.fromisoformat(last_updated)
+    logging.info("Retrieving cached data...")
+    cached_data = cosmosdb_callback_manager.get("discharges_cache")
+    last_updated = cosmosdb_callback_manager.get("last_updated")
 
-        if (
-            cached_data  # is not null
-            and last_updated  # is not null
-            and (now - last_updated)
-            < timedelta(seconds=auto_refresh_interval)  # within time window
-        ):
-            return cached_data, f"Last updated at {last_updated:%H:%M:%S}", False
+    if last_updated:
+        logging.info("Retrieved cached data.")
+        last_updated = datetime.fromisoformat(last_updated)
 
-        else:  # if after time window
-            df = _fetch_discharges()
+    time_since_last_update = now - last_updated if last_updated else None
 
-            cosmosdb_callback_manager.set(
-                cache_key="discharges_cache", value=df.to_dict("records")
-            )
-            cosmosdb_callback_manager.set(
-                cache_key="last_updated", value=now.isoformat()
-            )
+    # If data is already cached and within the refresh interval
+    if (
+        cached_data
+        and last_updated
+        and time_since_last_update < timedelta(seconds=AUTO_REFRESH_INTERVAL)
+        # ^^ should not be needed as expiry on cache
+    ):
+        return cached_data, f"Last updated at {last_updated:%H:%M:%S}", False
 
-        return df.to_dict("records"), f"Last updated at {last_updated:%H:%M:%S}", False
+    # Refresh data and update cache
+    df = _fetch_discharges()
+    logging.info("Refreshing cached data")
+    cosmosdb_callback_manager.set(
+        cache_key="discharges_cache", value=df.to_dict("records")
+    )
+    cosmosdb_callback_manager.set(cache_key="last_updated", value=now)
 
-
-# @callback(
-#     Output("dept_selector", "data"),
-#     Output("dept_selector", "value"),
-#     Input("campus_selector", "value"),
-# )
-# def _get_wards(campus_selector):
-#     campus_dict = {i.get("value"): i.get("label") for i in CAMPUSES}
-#     depts = (pd.read_json("assets/locations/departments.json")
-#              .query('closed_perm_01 != True')
-#              .query('department.str.startswith(@campus_dict[@campus_selector])')
-#              .department
-#              .unique()
-#              .tolist())
-#     return depts, depts[0]
+    return df.to_dict("records"), f"Last updated at {last_updated:%H:%M:%S}", False
